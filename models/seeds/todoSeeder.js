3332a9a06c68552a55ae9af5cdc89be0c9f3068f
dannychou <dannychou@jumbogames.com.tw>
2022-04-24 20:06:41 +0800

    refactor: modify mongoose config

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,17 +1,8 @@
-const mongoose = require('mongoose')
 const Todo = require('../todo.js')
+const db = require('../../config/mongoose')
 
-mongoose.connect(process.env.MONGODB_URI, {newUserUrlParser: true, useUnifiedTopology: true})
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('Error!')
-})
 
 db.once('open', () => {
-  console.log('MongoDB connected!')
-
   for (let i = 0; i < 10; i++) {
     Todo.create({name: `name-${i}`})
   }
