5cb6656a65bae7d46983692800190cdfbd252ae6
dannychou <dannychou@jumbogames.com.tw>
2022-04-24 16:23:43 +0800

    feat: model todo add isDone params

--- app.js
+++ app.js
@@ -73,10 +73,11 @@ app.get('/todos/:id/edit', (req, res) => {
 // update data
 app.post('/todos/:id/edit', (req, res) => {
   const id = req.params.id
-  const name = req.body.name
+  const {name, isDone} = req.body
   return Todo.findById(id)
     .then(todo => {
       todo.name = name
+      todo.isDone = isDone === 'on'
       return todo.save()
     })
     .then(() => res.redirect(`/todos/${id}`))
